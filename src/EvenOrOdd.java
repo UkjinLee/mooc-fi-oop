724cc608feeb904ca900a14207b40e265a19219f
asllan <asllan77@gmail.com>
2017-08-24 21:10:41 -0400

    solved exercise, even or odd

---
+++ src/EvenOrOdd.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+//Type a number: 2
+//Number 2 is even.
+//
+//Type a number: 7
+//Number 7 is odd.
+
+public class EvenOrOdd {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Type a number: ");
+        int number = reader.nextInt();
+        String evenOrOdd = number % 2 == 0 ? "even" : "odd";
+        System.out.println("Number " + number + " is " + evenOrOdd + ".");
+    }
+}
---
+++ templates/EvenOrOdd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+//Type a number: 2
+//Number 2 is even.
+//
+//Type a number: 7
+//Number 7 is odd.
+
+public class EvenOrOdd {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        // Type your program here
+    }
+}
