6bf7f5bb3585f96bab27541f6d3447807e8af3f7
asllan <asllan77@gmail.com>
2017-08-24 20:37:35 -0400

    solved Exercise 12: sum of the ages

--- templates/SumOfTheAges.java
+++ templates/SumOfTheAges.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
 
+//Type your name: Matti
+//Type your age: 14
+//
+//Type your name: Arto
+//Type your age: 12
+//
+//Matti and Arto are 26 years old in total.
+
 public class SumOfTheAges {
 
     public static void main(String[] args) {
