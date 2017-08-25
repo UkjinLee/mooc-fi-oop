ac93d8ee59127f3bdd1a96057f403bb8871b33b5
asllan <asllan77@gmail.com>
2017-08-25 16:16:59 -0400

    solved Exercise 40: PrintingLikeBoss

---
+++ templates/PrintingLikeBoss.java
@@ -0,0 +1,52 @@
+public class PrintingLikeBoss {
+
+    // copy or rewrite the method of Assignment 39.1 here
+    public static void printStars(int amount) {
+    }
+
+    public static void printWhitespaces(int amount) {
+        // 40.1
+    }
+
+//       *
+//      **
+//     ***
+//    ****
+
+    public static void printTriangle(int size) {
+        // 40.2
+    }
+
+//       *
+//      ***
+//     *****
+//    *******
+//      ***
+//      ***
+//    ---
+//               *
+//              ***
+//             *****
+//            *******
+//           *********
+//          ***********
+//         *************
+//        ***************
+//       *****************
+//      *******************
+//              ***
+//              ***
+
+    public static void xmasTree(int height) {
+        // 40.3
+    }
+
+    public static void main(String[] args) {
+        printTriangle(4);
+        System.out.println("---");
+
+        xmasTree(4);
+        System.out.println("---");
+        xmasTree(10);
+    }
+}
