001418ef1b9da1881eb75b1c3796e8d57c4e89ac
asllan <asllan77@gmail.com>
2017-08-25 13:16:24 -0400

    solved Exercise 26 - SumOfManyNumbers

---
+++ src/SumOfManyNumbers.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+//3
+//Sum now: 3
+//2
+//Sum now: 5
+//1
+//Sum now: 6
+//1
+//Sum now: 7
+//0
+//Sum in the end: 7
+
+public class SumOfManyNumbers {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int sum = 0;
+        
+        while (true) {
+            int read = Integer.parseInt(reader.nextLine());
+            if (read == 0) {
+                break;
+            }
+
+            sum += read;
+            System.out.println("Sum now: " + sum);
+        }
+        
+        System.out.println("Sum in the end: " + sum);
+    }
+}
