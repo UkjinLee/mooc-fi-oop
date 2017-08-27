93cdf7842b8bac15337a97a3b31f4b60d9bc7471
asllan <asllan77@gmail.com>
2017-08-27 19:41:03 -0400

    solved 41.GuessingNumberGame

--- src/GuessingNumberGame.java
+++ src/GuessingNumberGame.java
@@ -7,9 +7,22 @@ public class GuessingNumberGame {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         int numberDrawn = drawNumber();
+        int guessMade = 0;
 
-        // program your solution here. Do not touch the above lines!
-        
+        while(true) {
+            System.out.print("Guess a number: ");
+            int guess = reader.nextInt();
+            if (guess < numberDrawn)
+                System.out.print("The number is greater, ");
+            else if (numberDrawn < guess)
+                System.out.print("The number is lesser, ");
+            else
+                break;
+
+            guessMade++;
+            System.out.println("guesses made: " + guessMade);
+        }
+        System.out.println("Congratulations, your guess is correct!");
     }
 
     // DO NOT MODIFY THIS!
