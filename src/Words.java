64d063ccb764b35a04fd7cf03060ce8f2397430a
asllan <asllan77@gmail.com>
2017-08-28 16:44:12 -0400

    solved Exercise 57: Words

---
+++ src/Words.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//        Type a word: Mozart
+//        Type a word: Schubert
+//        Type a word: Bach
+//        Type a word: Sibelius
+//        Type a word: Liszt
+//        Type a word:
+//        You typed the following words:
+//        Mozart
+//        Schubert
+//        Bach
+//        Sibelius
+//        Liszt
+
+public class Words {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        ArrayList<String> words = new ArrayList<>();
+
+        while(true) {
+            System.out.print("Type a word: ");
+            String input = reader.nextLine();
+            if (input.isEmpty())
+                break;
+            words.add(input);
+        }
+
+        System.out.println("You typed the following words:");
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+}
---
+++ templates/Words.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//        Type a word: Mozart
+//        Type a word: Schubert
+//        Type a word: Bach
+//        Type a word: Sibelius
+//        Type a word: Liszt
+//        Type a word:
+//        You typed the following words:
+//        Mozart
+//        Schubert
+//        Bach
+//        Sibelius
+//        Liszt
+
+public class Words {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        ArrayList<String> words = new ArrayList<String>();
+    }
+}
