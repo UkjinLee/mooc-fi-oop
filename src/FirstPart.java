91eca1376993cf9bcf830b6035ba54016b522a16
asllan <asllan77@gmail.com>
2017-08-28 15:37:59 -0400

    solved 53.FirstPart

---
+++ src/FirstPart.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+//        Type a word: example
+//        Length of the first part: 4
+//        Result: exam
+//
+//        Type a word: example
+//        Length of the first part: 6
+//        Result: exampl
+
+public class FirstPart {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Type a word: ");
+        String word = reader.nextLine();
+
+        System.out.print("Length of the first part: ");
+        int length = reader.nextInt();
+
+        System.out.print("Result: " + word.substring(0, length));
+    }
+}
---
+++ templates/FirstPart.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+//        Type a word: example
+//        Length of the first part: 4
+//        Result: exam
+//
+//        Type a word: example
+//        Length of the first part: 6
+//        Result: exampl
+
+public class FirstPart {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+    }
+}
