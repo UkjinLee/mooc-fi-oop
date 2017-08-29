6a559e77958434093d0b7b08795dd75d075d86c6
asllan <asllan77@gmail.com>
2017-08-29 16:14:00 -0400

    solved Exercise 58: RecurringWord

---
+++ src/RecurringWord.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//        Type a word: carrot
+//        Type a word: celery
+//        Type a word: turnip
+//        Type a word: rutabaga
+//        Type a word: celery
+//        You gave the word celery twice
+
+public class RecurringWord {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        // create here the ArrayList 
+        ArrayList<String> words = new ArrayList<>();
+
+        String input;
+        while(true) {
+            System.out.print("Type a word: ");
+            input = reader.nextLine();
+            if (words.contains(input))
+                break;
+            words.add(input);
+        }
+
+        System.out.println("You gave the word " + input + " twice");
+    }
+}
---
+++ templates/RecurringWord.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//        Type a word: carrot
+//        Type a word: celery
+//        Type a word: turnip
+//        Type a word: rutabaga
+//        Type a word: celery
+//        You gave the word celery twice
+
+public class RecurringWord {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        // create here the ArrayList 
+        
+    }
+}
