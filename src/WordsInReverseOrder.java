14b675d4ec47a0767c6c66d357c0d116afd39da7
asllan <asllan77@gmail.com>
2017-08-29 16:29:13 -0400

    solved Exercise 59: Words in reverse order

---
+++ src/WordsInReverseOrder.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+//        Type a word: Mozart
+//        Type a word: Schubert
+//        Type a word: Bach
+//        Type a word: Sibelius
+//        Type a word: Liszt
+//        Type a word:
+//        You typed the following words:
+//        Liszt
+//        Sibelius
+//        Bach
+//        Schubert
+//        Mozart
+
+public class WordsInReverseOrder {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        ArrayList<String> words = new ArrayList<>();
+
+        while(true) {
+            System.out.print("Type a word: ");
+            String input = reader.nextLine();
+            if (input.isEmpty())
+                break;
+            words.add(input);
+        }
+
+        // todo: sort a word list
+        Collections.reverse(words);
+
+        System.out.println("You typed the following words:");
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+}
