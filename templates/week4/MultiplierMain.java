f9ff9cee29b91a2fed2d529fd74f9086f001f911
asllan <asllan77@gmail.com>
2017-08-31 19:12:24 -0400

    solved Exercise 74: Multiplier

---
+++ templates/week4/MultiplierMain.java
@@ -0,0 +1,18 @@
+public class MultiplierMain {
+
+//    threeMultiplier.multiply(2): 6
+//    fourMultiplier.multiply(2): 8
+//    threeMultiplier.multiply(1): 3
+//    fourMultiplier.multiply(1): 4
+
+    public static void main(String[] args) {
+        Multiplier threeMultiplier = new Multiplier(3);
+        System.out.println("threeMultiplier.multiply(2): " + threeMultiplier.multiply(2));
+
+        Multiplier fourMultiplier = new Multiplier(4);
+        System.out.println("fourMultiplier.multiply(2): " + fourMultiplier.multiply(2));
+
+        System.out.println("threeMultiplier.multiply(1): " + threeMultiplier.multiply(1));
+        System.out.println("fourMultiplier.multiply(1): " + fourMultiplier.multiply(1));
+    }
+}
