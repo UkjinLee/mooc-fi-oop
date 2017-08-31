762118c4bf8132bfb0fc34acb2caaedb88351f2e
asllan <asllan77@gmail.com>
2017-08-31 19:53:59 -0400

    solved Exercise 76: Menu

---
+++ templates/week4/Menu.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Menu {
+
+    private ArrayList<String> meals;
+
+    public Menu() {
+        this.meals = new ArrayList<String>();
+    }
+
+//    EXERCISE 76.1: ADDING A MEAL TO MENU
+    //
+    // adds a new meal to the list meals
+    // If the meal is already in the list, it should not be added.
+    public void addMeal(String meal) {
+
+    }
+
+//    EXERCISE 76.2: PRINTING THE MENU
+    //
+    // prints the meals in a menu
+    public void printMeals() {
+
+    }
+
+//    EXERCISE 76.3: CLEARING A MENU
+    //
+    public void clearMenu() {
+
+    }
+}
---
+++ templates/week4/MenuMain.java
@@ -0,0 +1,29 @@
+public class MenuMain {
+
+    public static void main(String[] args) {
+
+        Menu menu = new Menu();
+
+        //        EXERCISE 76.2: PRINTING THE MENU
+        //
+        //        Hamburger
+        //        Fish'n'Chips
+        //        Sauerkraut
+        //
+        System.out.println("-------------");
+        menu.addMeal("Hamburger");
+        menu.addMeal("Fish'n'Chips");
+        menu.addMeal("Sauerkraut");
+        menu.addMeal("Hamburger");
+        menu.printMeals();
+        System.out.println("-------------");
+        System.out.println();
+
+        // EXERCISE 76.3: CLEARING A MENU
+        //
+        System.out.println("-------------");
+        menu.clearMenu();
+        menu.printMeals();
+        System.out.println("-------------");
+    }
+}
