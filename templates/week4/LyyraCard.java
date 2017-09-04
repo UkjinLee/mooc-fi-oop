be6fda75d6cd5ec9dbfe75ef1d72bf51f6fe1d17
asllan <asllan77@gmail.com>
2017-09-04 10:51:27 -0400

    solved Exercise 77: Lyyra-card

---
+++ templates/week4/LyyraCard.java
@@ -0,0 +1,28 @@
+public class LyyraCard {
+
+    private double balance;
+
+    public LyyraCard(double balanceAtStart) {
+//        EXERCISE 77.1: CLASS SKELETON
+    }
+
+    // returns a string of the form "The card has X euros".
+    public String toString() {
+//        EXERCISE 77.1: CLASS SKELETON
+        return "";
+    }
+
+    public void payEconomical() {
+//        EXERCISE 77.2: PAYING WITH CARD
+//        decrease the balance by 2.50 euros
+    }
+
+    public void payGourmet() {
+//        EXERCISE 77.2: PAYING WITH CARD
+//        decrease the balance by 4.00 euros
+    }
+
+    public void loadMoney(double amount) {
+//        EXERCISE 77.4: LOADING MONEY TO CARD
+    }
+}
