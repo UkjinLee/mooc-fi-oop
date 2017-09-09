fbf96e7bd93d1648690d3b84bc13dd47b312bfca
asllan <asllan77@gmail.com>
2017-09-09 09:56:51 -0400

    solved Exercise 89: Clock

---
+++ templates/week5/ClockMain.java
@@ -0,0 +1,27 @@
+public class ClockMain {
+    public static void main(String[] args) {
+
+//        23:59:50
+//        23:59:51
+//        23:59:52
+//        23:59:53
+//        23:59:54
+//        23:59:55
+//        23:59:56
+//        23:59:57
+//        23:59:58
+//        23:59:59
+//        00:00:00
+//        00:00:01
+//        ...
+
+        Clock clock = new Clock(23, 59, 50);
+
+        int i = 0;
+        while( i < 20) {
+            System.out.println( clock );
+            clock.tick();
+            i++;
+        }
+    }
+}
