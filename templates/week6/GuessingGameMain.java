dc11cadb09dc71160934cfd5c66c9e7b9c219868
asllan <asllan77@gmail.com>
2017-09-21 17:15:10 -0400

    solved Exercise 105: Guessing game

---
+++ templates/week6/GuessingGameMain.java
@@ -0,0 +1,44 @@
+public class GuessingGameMain {
+
+    public static void main(String[] args) {
+
+//        EXERCISE 105.1: IS GREATER THAN
+//
+//        Is your number greater than 32? (y/n)
+//        y
+//        true
+//
+        GuessingGame game = new GuessingGame();
+        System.out.println(game.isGreaterThan(32));
+
+
+//        EXERCISE 105.2: AVERAGE
+//
+//        3
+//        9
+//
+//        GuessingGame game = new GuessingGame();
+//        System.out.println(game.average(3, 4));
+//        System.out.println(game.average(6, 12));
+
+
+//        EXERCISE 105.3: GUESSING LOGIC
+//
+//        Think of a number between 1...4.
+//        I promise you that I can guess the number you are thinking of with 2 questions.
+//
+//        Next I'll present you with a series of questions. Answer them honestly.
+//
+//        Is your number greater than 2? (y/n)
+//        y
+//        Is your number greater than 3? (y/n)
+//        y
+//        The number you're thinking of is 4.
+//
+//        GuessingGame game = new GuessingGame();
+//
+//        game.play(1,4);  // value to be guessed now within range 1-4
+//        game.play(1,10);  // value to be guessed now within range 1-10
+//        game.play(10,99);  // value to be guessed now within range 10-99
+    }
+}
