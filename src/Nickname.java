12b4ab2cc038a2f45c8ef0a0130a0cf89c831e33
asllan <asllan77@gmail.com>
2017-10-13 17:12:52 -0400

    solved Exercise 5: Nicknames

---
+++ src/Nickname.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Nickname {
+
+    public static void main(String[] args) {
+
+//        matti's nickname is mage
+//        mikael's nickname is mixu
+//        arto's nickname is arppa
+
+        Map<String, String> nicknames = new HashMap<>();
+        nicknames.put("matti", "mage");
+        nicknames.put("mikael", "mixu");
+        nicknames.put("arto", "arppa");
+        for (String name : nicknames.keySet()) {
+            System.out.println(name + "'s nickname is " + nicknames.get(name));
+        }
+    }
+}
---
+++ templates/week7/Nickname.java
@@ -0,0 +1,10 @@
+public class Nickname {
+
+    public static void main(String[] args) {
+
+//        matti's nickname is mage
+//        mikael's nickname is mixu
+//        arto's nickname is arppa
+                
+    }
+}
